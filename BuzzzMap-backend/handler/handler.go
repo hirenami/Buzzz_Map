93c3e32aa9be9f5fd2db84d2601169b5aa9cd5ad
Namito Hirezaki <7310wave@gmail.com>
2025-03-02 22:07:40 +0900

    handlerを定義

--- BuzzzMap-backend/handler/handler.go
+++ BuzzzMap-backend/handler/handler.go
@@ -5,7 +5,7 @@ import "github.com/gorilla/mux"
 func SetupRoutes(handler *Handler) *mux.Router {
 	r := mux.NewRouter()
 
-	
+	r.HandleFunc("/gettrend", handler.GetTrend).Methods("GET", "OPTIONS")
 
 	return r
 }
